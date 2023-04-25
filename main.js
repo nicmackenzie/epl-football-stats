cf449037834089233e4d513c7be3891e7b70eafb
nicmackenzie <nicmackenzie@live.com>
2023-04-25 13:09:11 +0300

    add event handlers for blur and change for input

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ themeToggle.addEventListener('click', () => {
   switchEl.classList.toggle('dark');
   changeTheme();
 });
+
+input.addEventListener('blur', e => {
+  if (e.target.value.trim() === '') {
+    input.classList.add('is-invalid');
+  }
+});
+
+input.addEventListener('change', e => {
+  if (e.target.value.trim() !== '') {
+    input.classList.remove('is-invalid');
+  }
+});
