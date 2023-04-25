8d52a90710609d0c883f1247dcaa9c08eec48d62
nicmackenzie <nicmackenzie@live.com>
2023-04-25 13:08:23 +0300

    Add sendRequest method

--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
 // variables
 const themeToggle = document.querySelector('.theme-toggle');
 const switchEl = document.querySelector('.switch');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const domainUrl = 'https://api-football-v1.p.rapidapi.com/v3';
 
 //animate tranistion between themes
 function transition() {
@@ -19,4 +22,14 @@ function changeTheme() {
     transition();
     document.documentElement.setAttribute('data-theme', 'light');
   }
+}
+
+async function sendRequest(url) {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '915f65df84msh2ef701f1718c032p1f5431jsnfed3d971dc76',
+    },
+  });
+  return await response.json();
 }
