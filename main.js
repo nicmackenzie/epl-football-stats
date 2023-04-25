8d98b0f01ef57cf37eda99de9031e85f5c3ce472
nicmackenzie <nicmackenzie@live.com>
2023-04-25 16:55:32 +0300

    HTML Render for club info and match stats

--- main.js
+++ main.js
@@ -23,7 +23,9 @@ form.addEventListener('submit', e => {
     return;
   }
   mainContent.innerHTML = '';
+  // input.value = '';
   fetchData(input.value.trim());
+  input.value = '';
 });
 
 async function fetchData(name) {
@@ -45,7 +47,7 @@ async function fetchData(name) {
     getTopScorers(team.secondaryId),
   ]);
   removeSpinner();
-  console.log(stats, rank, scorers);
+  console.log(scorers);
   renderHTML(info, stats, rank, scorers, team.secondaryId);
 }
 
@@ -57,7 +59,9 @@ function renderHTML(info, stats, rank, scorers, secondaryId) {
   const html = `
     <div class="club-information">
       <div class="flex">
-        <img src="${info.response[0].team.logo}" alt="${info.response[0].team.name}" />
+        <img src="${info.response[0].team.logo}" alt="${
+    info.response[0].team.name
+  }" />
         <div class="about-club">
           <h2>${info.response[0].team.name}</h2>
           <p>Founded in ${info.response[0].team.founded}</p>
@@ -69,7 +73,91 @@ function renderHTML(info, stats, rank, scorers, secondaryId) {
           <span class="rank">${pointsRanks.rank}</span>
           <span class="points">${pointsRanks.points} points</span>
       </div>
-    </div>  
+      <div class="self-center">
+         <p class="club-form">Club Form (Last 10 matches)</p>
+         <div class="flex gap">
+           ${getLastTen(stats.response.form)
+             .split('')
+             .map(form => {
+               return `<div class="form ${form.toLowerCase()}">${form}</div>`;
+             })
+             .join('')}
+         </div>
+      </div>
+    </div>
+    <div class="match-stats">
+      <div class="stat-card">       
+        <div class="title">Games Played</div>
+        <div class="content">
+          <div class="stat">
+            <p>Wins</p>
+            <span>${stats.response.fixtures.wins.total}</span>
+          </div>
+          <div class="stat">
+            <p>Draws</p>
+            <span>${stats.response.fixtures.draws.total}</span>
+          </div>
+          <div class="stat">
+            <p>Losses</p>
+            <span>${stats.response.fixtures.loses.total}</span>
+          </div>   
+        </div>
+      </div>
+      <div class="stat-card">
+        <div class="title">Goals Scored</div>
+          <div class="content">
+            <div class="stat">
+              <p>For</p>
+              <span>${stats.response.goals.for.total.total}</span>
+            </div>
+            <div class="stat">
+              <p>Againist</p>
+              <span>${stats.response.goals.against.total.total}</span>
+            </div>
+            <div class="stat">
+              <p>Goal Difference</p>
+              <span>${
+                +stats.response.goals.for.total.total -
+                stats.response.goals.against.total.total
+              }</span>
+            </div>
+          </div>
+      </div>
+      <div class="stat-card">
+        <div class="title">Clean Sheets</div>
+          <div class="content">
+            <div class="stat">
+              <p>Home</p>
+              <span>${stats.response['clean_sheet'].home}</span>
+            </div>
+            <div class="stat">
+              <p>Away</p>
+              <span>${stats.response['clean_sheet'].away}</span>
+            </div>
+            <div class="stat">
+              <p>Total</p>
+              <span>${stats.response['clean_sheet'].total}</span>
+            </div>
+          </div>
+      </div>
+      <div class="stat-card">
+        <div class="title">Failed To Score</div>
+        <div class="content">
+          <div class="stat">
+            <p>Home</p>
+            <span>${stats.response['failed_to_score'].home}</span>
+          </div>
+          <div class="stat">
+            <p>Away</p>
+            <span>${stats.response['failed_to_score'].away}</span>
+          </div>
+          <div class="stat">
+            <p>Total</p>
+            <span>${stats.response['failed_to_score'].total}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   `;
   mainContent.innerHTML = html;
 }
